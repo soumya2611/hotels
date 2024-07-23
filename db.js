58ccabb8aed8f56da76c035da58ed75dc50ef10f
soumya2611 <workforsoumya21@gmail.com>
2024-07-23 19:58:45 +0530

    added middleware

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const mongoURL = process.env.MONGODB_URL_LOCAL
-//const mongoURL=process.env.MONGODB_URL
+//const mongoURL = process.env.MONGODB_URL_LOCAL
+const mongoURL=process.env.MONGODB_URL
 //Connection setUp
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
